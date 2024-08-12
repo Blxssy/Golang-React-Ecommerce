d5be0e40476d447099a1a70073e884101ed14485
D13tre33 <bebrochk4@gmail.com>
2024-08-12 12:36:50 +0300

    amogus

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ProductsList } from '../components/ProductsList';
+
 import '../styles/Home.css';
 
 const Home = () => {
 	return (
 		<div className='home'>
 			<h1>Products List</h1>
-			<ProductsList/>
+
 		</div>
 	);
 };
