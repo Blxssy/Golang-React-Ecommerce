25eade285e1a92bfdcd188ef6a6cf0d4d2d60d52
D13tre33 <bebrochk4@gmail.com>
2024-08-12 18:41:04 +0300

    homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
 import '../styles/Home.css';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<h1>Products List</h1>
-
+			<div className='banner'>
+				<h1>Welcome to Our Store</h1>
+				<p>Discover amazing products we created just for you</p>
+			</div>
+			<section className='products'>
+				<h2>Products List</h2>
+				<div className='product-grid'>
+					{/* Product items will go here */}
+					<div className='product-item'>Product 1</div>
+					<div className='product-item'>Product 2</div>
+					<div className='product-item'>Product 3</div>
+					<div className='product-item'>Product 4</div>
+				</div>
+			</section>
 		</div>
 	);
 };
